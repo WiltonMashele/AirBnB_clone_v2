a94dadf9d096a78af574f673a4f0a6d92cb3120d
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-05 06:11:23 -0800

    script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A simple Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_hbnb():
+    """This function handles the root URL and returns a greeting."""
+    return 'Hello, HBNB!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
