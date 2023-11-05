3d0fb894e3ed58b33a18eec3185677994e11c1b5
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-05 06:53:28 -0800

    fixing

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
 
+
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello_hbnb():
