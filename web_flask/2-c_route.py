a6d4d4d5deebd27b84d1e3b07a8660782820167a
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-05 06:50:07 -0800

    a script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_hbnb():
+    """hello_hbnb"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    """hbnb"""
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_with_variable(text):
+    """c_with_variable"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
