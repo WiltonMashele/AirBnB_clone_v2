6debf947c3b82b63cf313fba8c13d6c2ef453ea5
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-05 15:20:54 -0800

    fixing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
 sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install nginx -y
+sudo apt-get install -y nginx
+
 sudo mkdir -p /data/web_static/{releases,test,shared}
 echo "Test deploying web_static" | sudo tee /data/web_static/releases/test/index.html
-sudo sed -i '/^\tserver_name/ a\\tlocation /hbnb_static \{\n\t\talias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+nginx_config="/etc/nginx/sites-available/default"
+nginx_config_location="/etc/nginx/sites-available/hbnb_static"
+if [ ! -f "$nginx_config_location" ]; then
+    echo "location /hbnb_static {" | sudo tee "$nginx_config_location"
+    echo "    alias /data/web_static/current;" | sudo tee -a "$nginx_config_location"
+    echo "}" | sudo tee -a "$nginx_config_location"
+    sudo ln -s "$nginx_config_location" "/etc/nginx/sites-enabled/hbnb_static"
+fi
+
 sudo service nginx restart
