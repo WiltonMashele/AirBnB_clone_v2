82479c4982b5bb356466da16f4944d4f74bd16bc
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-05 06:58:18 -0800

    fixing

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,10 +3,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_hbnb():
     """Route for '/' that displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', methods=['GET'], strict_slashes=False)
 def hbnb():
