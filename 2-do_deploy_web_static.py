ba3d82f0bf723fcc6d9de0a319069841d66565a2
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-06 12:00:55 -0800

    fixing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,43 +2,33 @@
 """
 Distribute an archive to web servers using Fabric.
 """
-from fabric.api import env, put, run, sudo
-from os.path import isfile
+from datetime import datetime
+from fabric import Connection
+import os
 
-env.hosts = ['<IP web-01>', '<IP web-02>']
-env.user = '<username>'
-env.key_filename = '<SSH key path>'  # Replace with your SSH key path
+
+env.hosts = ["172.17.0.9", "172.17.255.255"]
+env.user = "ubuntu"
+
+def do_pack():
+    """
+    Create an archive and return its path if it's generated successfully.
+    """
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_file_path = "versions/web_static_{}.tgz".format(date)
+    result = local("tar -czvf {} web_static".format(archived_file_path))
+
+    if result.succeeded:
+        return archived_file_path
+    else:
+        return None
 
 def do_deploy(archive_path):
     """
     Distribute an archive to web servers.
-
-    Args:
-        archive_path: Path to the archive file to deploy.
-
-    Returns:
-        True if successful, False otherwise.
     """
-    if not isfile(archive_path):
-        return False
-
-    # Get the filename without extension
-    archive_filename = archive_path.split("/")[-1].split(".")[0]
-
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, "/tmp/")
-
-    # Uncompress the archive to the folder /data/web_static/releases/<archive_filename>
-    run("mkdir -p /data/web_static/releases/{}".format(archive_filename))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_filename + ".tgz", archive_filename))
-
-    # Delete the archive from the web server
-    run("rm /tmp/{}".format(archive_filename + ".tgz"))
-
-    # Delete the symbolic link /data/web_static/current
-    run("rm /data/web_static/current")
-
-    # Create a new symbolic link /data/web_static/current linked to the new version
-    run("ln -s /data/web_static/releases/{} /data/web_static/current".format(archive_filename))
-
-    return True
+    if os.path.exists(archive_path):
+        archived_file = os.path.basename(archive_path)
+        newest_version = "/data/web_static/releases/{}".format(archived_file[:-4])
+        archived_file = "/tmp/{}".format(archived_file)
